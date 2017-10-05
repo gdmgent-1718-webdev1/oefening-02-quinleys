185ace1d45864659eb15394ef743f36a41ffc5fb
quinleys <quinleys@student.arteveldehs.be>
2017-10-05 11:40:40 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+    $string1 = 'Hello';
+    $string2 = 'World';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Oefening 2</title>
+</head>
+<body>
+    <p><?php echo $string1 . ' ' . $string2; ?> </p>
+</body>
+</html>
